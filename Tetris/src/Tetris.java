65de422e23fc1b97ff955f0ae8ecc18a7285e314
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-07-12 16:56:43 +0300

    Completed 3 task and added 4. Added 2 var field and figure. Added getters.

--- Tetris/src/Tetris.java
+++ Tetris/src/Tetris.java
@@ -1,4 +1,15 @@
 public class Tetris {
+    Field field;
+    Figure figure;
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+
     public static void main(String[] args) {
 
     }
