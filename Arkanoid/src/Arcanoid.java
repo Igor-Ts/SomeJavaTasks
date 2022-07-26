187121f8094cbf333d7f06050cc83610ef228e3b
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-07-26 17:11:08 +0300

    Completed task 3. Added 2 int variables, constructor, getter and setter. Added task 4.

--- Arkanoid/src/Arcanoid.java
+++ Arkanoid/src/Arcanoid.java
@@ -1,4 +1,28 @@
 public class Arcanoid {
+    private int width;
+    private int height;
+
+    public Arcanoid(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
     public static void main(String[] args) {
 
     }
