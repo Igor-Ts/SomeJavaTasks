e0866146ddfbd49202e534c165928041439c03b3
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-07-26 19:51:04 +0300

    Completed task 7. Into BaseObject class was added new variables and it's setters, getters and constructor. Added task 8.

--- Arkanoid/src/Ball.java
+++ Arkanoid/src/Ball.java
@@ -1,2 +1,6 @@
 public class Ball extends BaseObject {
+
+    public Ball(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
--- Arkanoid/src/Stand.java
+++ Arkanoid/src/Stand.java
@@ -1,2 +1,6 @@
 public class Stand extends BaseObject{
+
+    public Stand(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
