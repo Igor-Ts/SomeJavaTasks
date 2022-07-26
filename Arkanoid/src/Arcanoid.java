2ce0f3b0fa1b65adbaf69e45fb7e412f62fe99ae
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-07-26 17:53:23 +0300

    Completed task 4. Added 2 object variables and array list. Added task 5.

--- Arkanoid/src/Arcanoid.java
+++ Arkanoid/src/Arcanoid.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
+
 public class Arcanoid {
+
+    private Ball ball;
+    private Stand stand;
+
+    private ArrayList<Brick> bricks;
+
     private int width;
     private int height;
 
@@ -21,6 +29,30 @@ public class Arcanoid {
 
     public int getHeight() {
         return height;
+    }
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public ArrayList<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public void setBricks(ArrayList<Brick> bricks) {
+        this.bricks = bricks;
     }
 
     public static void main(String[] args) {
