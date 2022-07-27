ee64893f143ad6d0fe2b1e5fefbd9119e2262f77
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-07-27 18:17:26 +0300

    Completed task 10. Into class Canvas added 2 methods setPoint and drawMatrix. Added these methods' realization. Added task 11.

--- Arkanoid/src/Canvas.java
+++ Arkanoid/src/Canvas.java
@@ -9,6 +9,28 @@ public class Canvas {
         this.width = width;
         this.height = height;
     }
+// Put one point on the canvas with coordinates (x,y) and color - c.
+    public void setPoint (double x, double y, char c) {
+        int x0 = (int) Math.round(x);
+        int y0 = (int) Math.round(y);
+        if (x0 < 0 || y0 < 0 || y0 > matrix.length || x0 > matrix[y0].length) {
+            return;
+        } else {
+            matrix [x0][y0] = c;
+        }
+    }
+// draw the passed figure in the pointed coordinates with c color
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        int height = matrix.length;
+        int width = matrix[0].length;
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (matrix[i][j] != 0){
+                    setPoint(x+j, y+i, c);
+                }
+            }
+        }
+    }
 
     public char[][] getMatrix() {
         return matrix;
