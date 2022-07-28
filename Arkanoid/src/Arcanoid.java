8497f643c03c0e65c6d40f1cfe238dcf4004d4a2
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-07-28 21:46:50 +0300

    Completed task 18. Class Arcanoid:realized methods move, draw, run and main. Added task 19.

--- Arkanoid/src/Arcanoid.java
+++ Arkanoid/src/Arcanoid.java
@@ -1,3 +1,4 @@
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 public class Arcanoid {
@@ -17,15 +18,84 @@ public class Arcanoid {
         this.height = height;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        game = new Arcanoid(20, 30);
 
+        Ball ball = new Ball(10, 29, 2,  95);
+        game.setBall(ball);
+
+        Stand stand = new Stand(10, 30);
+        game.setStand(stand);
+
+        game.getBricks().add(new Brick(3, 3));
+        game.getBricks().add(new Brick(7, 5));
+        game.getBricks().add(new Brick(12, 5));
+        game.getBricks().add(new Brick(16, 3));
+
+        game.run();
     }
 
-    public void run () {
+    public void run () throws Exception {
+        //create canvas to draw.
+        Canvas canvas = new Canvas(width, height);
 
+        //create object observer and start them
+        KeyboardObserver keyboardObserver = new KeyboardObserver();
+        keyboardObserver.start();
+
+        //Execute the cycle while the game is not ended
+        while (!isGameOver)
+        {
+            //"observer" contains events about key tapping?
+            if (keyboardObserver.hasKeyEvents())
+            {
+                KeyEvent event = keyboardObserver.getEventFromTop();
+
+                //if "left" - move figure to the left
+                if (event.getKeyCode() == KeyEvent.VK_LEFT)
+                    stand.moveLeft();
+                    //if "right" - move figure to the right
+                else if (event.getKeyCode() == KeyEvent.VK_RIGHT)
+                    stand.moveRight();
+                    //if "space" - ball start
+                else if (event.getKeyCode() == KeyEvent.VK_SPACE)
+                    ball.start();
+            }
+
+            //move all objects
+            move();
+
+            //check bumping
+            checkBricksBump();
+            checkStandBump();
+
+            //check may the ball to fly through bottom
+            checkEndGame();
+
+            //draw all objects
+            canvas.clear();
+            draw(canvas);
+            canvas.print();
+
+            //pause
+            Thread.sleep(300);
+        }
+
+        //"Game Over"
+        System.out.println("Game Over!");
     }
 
     public void move() {
+        stand.move();
+        ball.move();
+    }
+
+    public void draw (Canvas canvas) {
+        ball.draw(canvas);
+        stand.draw(canvas);
+        for (Brick br: bricks) {
+            br.draw(canvas);
+        }
 
     }
 
