440f4bbe23d6a02615151899c62b6de8d1d0574e
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-08-20 15:14:20 +0300

    Completed task 2. Added 2 exceptions and 2 static methods to show message and console read. Added task 3.

--- CashMachine/src/ConsoleHelper.java
+++ CashMachine/src/ConsoleHelper.java
@@ -1,3 +1,22 @@
-public class ConsoleHelper {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class ConsoleHelper {
+    public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String message = " ";
+        try {
+            message = reader.readLine();
+        } catch (IOException ignored){
+
+        }
+
+        return message;
+    }
 }
---
+++ CashMachine/src/exception/NotEnoughMoneyException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class NotEnoughMoneyException {
+
+    public NotEnoughMoneyException() {
+
+    }
+}
