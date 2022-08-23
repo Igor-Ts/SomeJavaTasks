4b51e22ab9942e5657148fa7d9798394556d626f
Igor-Ts <igor.tsepulkovski@gmail.com>
2022-08-23 19:41:56 +0300

    Completed task 4. Realized deposit operation, added methods: askCurrencyCode, getValidTwoDigits, addAmount. Added these methods' realization. Added task 5.

--- CashMachine/src/CashMachine.java
+++ CashMachine/src/CashMachine.java
@@ -1,5 +1,8 @@
+import java.util.Locale;
+
 public class CashMachine {
     public static void main(String[] args) {
-
+        Locale.setDefault(Locale.ENGLISH);
+        ConsoleHelper.getValidTwoDigits(ConsoleHelper.askCurrencyCode());
     }
 }
--- CashMachine/src/ConsoleHelper.java
+++ CashMachine/src/ConsoleHelper.java
@@ -16,7 +16,43 @@ public class ConsoleHelper {
         } catch (IOException ignored){
 
         }
-
         return message;
+    }
+
+    public static String askCurrencyCode() {
+        String currencyCode;
+        writeMessage("Please, write currency code(EUR, USD, RUB etc.)");
+        currencyCode = readString();
+        if (currencyCode.length() == 3) {
+            return currencyCode.toUpperCase();
+        } else {
+            writeMessage("Invalid code");
+            askCurrencyCode();
+        }
+        return currencyCode.toUpperCase();
+    }
+
+    public static String[] getValidTwoDigits(String currencyCode) {
+        String [] array;
+        while(true) {
+            writeMessage("Please, write denomination after put spacebar and write number of currencies (ex. 200 1200)");
+            int denomination;
+            int count;
+            String value = readString();
+            array = value.split(" ");
+            try {
+                denomination = Integer.parseInt(array[0]);
+                count = Integer.parseInt(array[1]);
+            } catch (Exception e) {
+                writeMessage("This data is incorrect");
+                continue;
+            }
+            if (denomination <=0 || count <=0 || array.length > 2){
+                writeMessage("This data is incorrect");
+                continue;
+            }
+            break;
+        }
+        return array;
     }
 }
